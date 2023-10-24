cb5283a4c0e0fc78a436d0f9b3bd2dbe2c8d4c44
DaniilPi <mr.daniil.aleksandrovich@mail.ru>
2023-10-24 21:45:05 +0300

    Домашка к 5 семинару

---
+++ fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Числа Фибоначчи
+*/
+
+Console.Clear();
+
+System.Console.Write("Введите число: ");
+int numb = int.Parse(Console.ReadLine());
+
+int Fi(int n)
+{
+    if (n == 0)
+    return 0;
+    if (n == 1)
+    return 1;
+    else
+    return Fi(n-1) + Fi(n-2);
+}
+
+for (int i = 0; i <= numb; i++)
+{
+    System.Console.WriteLine($"{i}! = {Fi(i)}");
+}
---
+++ task36/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Напишите программу, которая находит сумму элементов с нечетными индексами 
+в одномерном массиве целых чисел и выводит результат на экран.
+*/
+
+public class Answer
+{
+    public static int SumOddElements(int[] array)
+    {
+        // Введите свое решение ниже
+        int Sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Sum += array[i];
+            }
+        
+        }
+        return Sum;
+
+
+    }
+
+    public static void PrintArray(int[] array)
+    {
+         // Введите свое решение ниже
+            for (int i = 0; i < array.Length; i++)
+        {  
+            if (i == array.Length - 1)
+            System.Console.Write(array[i]);
+            else
+            System.Console.Write(array[i] + "\t");
+        }
+        System.Console.WriteLine();
+
+    }
+
+
+// Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args)
+    {
+        int[] array;
+        if (args.Length == 0)
+        {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            array = new int[] { 12, 64, 28, 93, 35, 47, 6, 72, 58, 21 };
+        }
+        else
+        {
+            string[] argStrings = args[0].Split(", ");
+            array = new int[argStrings.Length];
+            for (int i = 0; i < argStrings.Length; i++)
+            {
+                if (int.TryParse(argStrings[i], out int number))
+                {
+                    array[i] = number;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+                    return;
+                }
+            }
+        }
+
+        // Не удаляйте строки ниже
+        PrintArray(array);
+        int sumOdd = SumOddElements(array);
+        Console.WriteLine($"Сумма нечетных элементов: {sumOdd}");
+    }
+}
